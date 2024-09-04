a0c13c536af055a32beb3e139797ed8985914076
GtOliv3r <123522878+GtOliv3r@users.noreply.github.com>
2024-09-03 22:37:08 -0300

    feat: add Infra layer

---
+++ CleanMovie.Infrastructure/MovieRepository.cs
@@ -0,0 +1,20 @@
+﻿using CleanMovie.Application;
+using CleanMovie.Domain;
+
+namespace CleanMovie.Infrastructure
+{
+    public class MovieRepository : IMovieRepository
+    {
+        public static IList<Movie> movies = new List<Movie>()
+        {
+        new Movie {Id = 1, Title = "Homem de Ferro",Description = "Homem que usa armadura do Iron Golen",ProductionCompany = "Marvel Studios"},
+        new Movie { Id = 2, Title = "Homem de Ferro 2", Description = "Homem com usa armadura do Gold Golem", ProductionCompany = "Marvel Studios" },
+        }; 
+        
+
+        public IList<Movie> GetAllMovies() 
+        {
+            return movies;
+        }
+    }
+}
