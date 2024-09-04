4c4287fde12aab86d641e5149c616d555f683ab1
GtOliv3r <123522878+GtOliv3r@users.noreply.github.com>
2024-09-03 22:36:50 -0300

    feat: add Application layer

---
+++ CleanMovie.Application/IMovieRepository.cs
@@ -0,0 +1,9 @@
+﻿using CleanMovie.Domain;
+
+namespace CleanMovie.Application
+{
+    public interface IMovieRepository
+    {
+        IList<Movie> GetAllMovies();
+    }
+}
---
+++ CleanMovie.Application/MovieService.cs
@@ -0,0 +1,21 @@
+﻿using CleanMovie.Domain;
+
+namespace CleanMovie.Application
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        //Construtor para fazer injeção de dependencia
+        public MovieService(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+        public IList<Movie> GetAllMovies()
+        {
+            IList<Movie> movies = _movieRepository.GetAllMovies();
+
+            return movies;
+        }
+    }
+}
