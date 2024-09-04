0f82460457d77ec287c40aafa5a7681cf45060e4
GtOliv3r <123522878+GtOliv3r@users.noreply.github.com>
2024-09-03 22:36:06 -0300

    feat: add domain layer

---
+++ CleanMovie/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace CleanMovie.Domain
+{
+    public class Movie
+    {
+        public int Id { get; set; } 
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ProductionCompany { get; set; }
+    }
+}
