6ae1a2a0dc543eaddf5442010307c340724e5a2e
GtOliv3r <123522878+GtOliv3r@users.noreply.github.com>
2024-09-03 22:36:50 -0300

    feat: add Application layer

---
+++ CleanMovie.Application/IMovieService.cs
@@ -0,0 +1,12 @@
+﻿using CleanMovie.Domain;
+
+namespace CleanMovie.Application
+{
+    /// <summary>
+    /// Essa classe representa os Uses Cases
+    /// </summary>
+    public interface IMovieService
+    {
+        IList<Movie> GetAllMovies();
+    }
+}
